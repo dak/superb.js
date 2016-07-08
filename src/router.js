619bb30511849fa9ef285d5c7b2962126469d1ab
Derek Kent <dak@dak.org>
2016-07-08 17:00:15 -0400

    Match routes exactly by default

--- src/router.js
+++ src/router.js
@@ -7,20 +7,26 @@ let defaultRoute  = /.*/,
     splatParam    = /\*\w+/g,
     escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g;
 
+const CONVERT_ROUTE = Symbol();
 const LOAD_ROUTE = Symbol();
 const RESTORE_WINDOW_POSITION = Symbol();
 
 class Route {
 
     constructor(path, options = {}, router) {
-        this.path = path;
-        this.router = router;
+        if (!(path instanceof RegExp)) {
+            options.view = options.view || path;
+            path = Route[CONVERT_ROUTE](path);
+        }
 
         if (typeof options === 'string') {
             this.options = {view: options};
         } else {
             this.options = options;
         }
+
+        this.path = path;
+        this.router = router;
 
         if (path === defaultRoute) {
             router.defaultRoute = this;
@@ -38,7 +44,7 @@ class Route {
 
         if (this.onload) {
             this.onload(state, params).then(() => {
-                this[RESTORE_WINDOW_POSITION]();
+                Route[RESTORE_WINDOW_POSITION]();
             });
 
             return;
@@ -55,7 +61,7 @@ class Route {
                 const Controller = m.default;
 
                 router.defaultRegion.attach(new Controller(params));
-                this[RESTORE_WINDOW_POSITION]();
+                Route[RESTORE_WINDOW_POSITION]();
                 router.loadFailure = false;
             }).catch((e) => {
                 if (!router.loadFailure && router.defaultRoute instanceof Route) {
@@ -68,18 +74,25 @@ class Route {
         }
     }
 
-    [RESTORE_WINDOW_POSITION]() {
+    static [RESTORE_WINDOW_POSITION]() {
         if (typeof history.state === 'object' && history.state !== null) {
             window.scrollTo(history.state.x || 0, history.state.y || 0);
         } else {
             window.scrollTo(0, 0);
         }
+    }
 
+    static [CONVERT_ROUTE](route) {
+        route = route.replace(escapeRegExp, '\\$&')
+                     .replace(optionalParam, '(?:$1)?')
+                     .replace(namedParam, (match, optional) => optional ? match : '([^/?]+)')
+                     .replace(splatParam, '([^?]*?)');
+
+        return new RegExp(`^\/${route}$`);
     }
 
 }
 
-const CONVERT_ROUTE = Symbol();
 const ON_POPSTATE = Symbol();
 const POPSTATE_EVENT_LISTENER = Symbol();
 const SCROLL_EVENT_LISTENER = Symbol();
@@ -98,11 +111,6 @@ class Router {
     route(route, options = {}) {
         if (typeof options === 'string') {
             options = {view: options};
-        }
-
-        if (!(route instanceof RegExp)) {
-            options.view = options.view || route;
-            route = this[CONVERT_ROUTE](route);
         }
 
         return new Route(route, options, this);
@@ -196,15 +204,6 @@ class Router {
         if (!match && this.defaultRoute instanceof Route) {
             this.defaultRoute[LOAD_ROUTE](state, params);
         }
-    }
-
-    [CONVERT_ROUTE](route) {
-        route = route.replace(escapeRegExp, '\\$&')
-                     .replace(optionalParam, '(?:$1)?')
-                     .replace(namedParam, (match, optional) => optional ? match : '([^/?]+)')
-                     .replace(splatParam, '([^?]*?)');
-
-        return new RegExp(`^\/${route}`);
     }
 
 }
