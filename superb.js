943374ad8bb5d2b33a6e97ebc53fd48fea519666
Derek Kent <dak@dak.org>
2016-07-20 17:45:15 -0400

    Remove Model

--- superb.js
+++ superb.js
@@ -1,6 +1,5 @@
-import Model from './src/model';
 import Controller from './src/controller';
 import Router from './src/router';
 import utils from './src/utils';
 
-export {Model, Controller, Router, utils};
+export {Controller, Router, utils};
